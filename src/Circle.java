3d42bc9f50c91df90c17c69aeb0dd01a096f0a2d
kiran1090 <63589265+kiran1090@users.noreply.github.com>
2022-12-28 10:26:16 +0530

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -33,11 +33,12 @@ public class Circle {
         {
           return color;
         }
-
+// Setter for instance variable radius
      public void setRadius(double newRadius)
      {
          radius=newRadius;
      }
+     // Setter for instance variable color
      public void setColor(String newColor)
      {
          color=newColor;
